588881c1beb9c197de865515e9f42a0262aa421f
Claudio Pacchiega <claudio.pacchiega@valuepartners.com>
2021-11-23 17:32:56 +0100

    now it should be OK it is attached to infotext so that kids can see the name associated while pointing at the block and clicking it appears and now it is possible to change it every block can point to a different word

--- init.lua
+++ init.lua
@@ -1,32 +1,16 @@
 MOD_NAME = minetest.get_current_modname();
 MOD_PATH = minetest.get_modpath(MOD_NAME);
 --require("debugger")("127.0.0.1", 10000,"luaidekey")
-
--- request http_api for being able to send httprequest
--- note you MUST give the http
 local http_api = minetest.request_http_api and minetest.request_http_api()
 if not http_api then
   minetest.log("MOD ${mod}: ERROR You must provide http right to this mod to work" % {mod = MOD_NAME})
   minetest.log("===================================================================")
   return false
 end
-
-
---require("mobdebug").start()
 dofile(MOD_PATH.."/basic.lua")
 dofile(MOD_PATH.."/utf8.lua")
 
-
---
--- local environment is held for each position
---
-
-
---
--- globals for this mod for handling general constants
---
 local vikidia = {
-  nodeEnvs = {},
   formname = "vikidia.prova",
   --site = "it.vikidia.org",
   site = 'it.wikipedia.org',
@@ -41,87 +25,50 @@ local vikidia = {
     button[6.7,2.5;3,1.1;;Search]
     field[1.4,2.6;5,0.8;search;search;${search}]
     textarea[0.9,4.5;9,5.9;;;${desc}]
-    textarea[0.5,10.9;9.8,0.9;position;                                                POS;${position}]
+    textarea[0.5,10.9;9.8,0.9;position;;${position}]
     ]],
   api = http_api,
-
 }
 
-
--- whenever a form with our name is filled by a player
-
-minetest.register_on_player_receive_fields(function(player, formname, fields)
-  -- be sure to check this is really our formname to not pollute and receive others
-  if formname ~= vikidia.formname then
-    return false
-  end
-  local nodepos = fields.position
-  -- if exit button just exits
-  if fields.exit then
-    minetest.log("form quit")
-    return false
-  end
-
-  -- try to persist the searches and description made by the same player
-  -- this can be changed to persist this information on the position instead of
-  -- the player
-  local player_name = player:get_player_name()
-  
-  local key = fields.position
-  if not key then 
-    return
-  end
-  local env = vikidia.nodeEnvs[key];
-  if not env then
-    env={}
-    vikidia.nodeEnvs[key]=env
-  end
-
-  -- compose the formspec with search and previous fetched information
-  env.search = fields.search
-  env.position = key
-
+local function callWikipedia(search)
   local url = vikidia.composeUrl %
     { site = vikidia.site,
-      title = urlencode(env.search)
+      title = urlencode(search)
     }
+  local handle = vikidia.api.fetch_async({url = url, timeout = 10})
+  local result
+  repeat
+    result = vikidia.api.fetch_async_get(handle)
+  until (result.completed)
 
-  -- when http finishes we must collect the data
-  local function fetch_callback(result)
-    if not result.completed then
-      minetest.log("request still uncomplete")
-      return
-    end
-    minetest.log("finished request")
-    -- extract only what appears after extract
-    local desc = result.data:match('.*extract":"(.*)"}}}}')
-    -- only if some information extracted
-    if(not desc) then
-      desc = "Questa voce su Vikidia non esiste, prova a scriverla!"
-    end
-    -- be sure to replace newlines with separators
-    -- and to interpret correctly json handling utf8
-    env.desc = string.gsub(getUtf8(desc),"\\n"," ")
+  -- extract only what appears after extract
+  local desc = result.data:match('.*extract":"(.*)"}}}}')
+  return string.gsub(getUtf8(desc),"\\n"," ")
+end
 
-    -- show the search and what found in the form with scrolling bars if needed
-    local formspec = vikidia.composeFormspec %
-      {
-        position = env.position,
-        search = env.search,
-        desc = minetest.formspec_escape(env.desc)
-      }
+local function getInfoText(position)
+  local nodepos = minetest.string_to_pos(position)
+  local meta = minetest.get_meta(nodepos)
+  local desc = meta.get_string(meta,"infotext")
+  return desc
+end
 
-    minetest.show_formspec(player_name, vikidia.formname, formspec)
+local function setInfoText(position,value)
+  local nodepos = minetest.string_to_pos(position)
+  local meta = minetest.get_meta(nodepos)
+  meta.set_string(meta,"infotext",value)
+end
 
-  end
-
-  -- this is the actual http call
-  http_api.fetch({url = url, timeout = 10}, fetch_callback)
-
-
-end)
-
-
+local function showForm(player_name,position,search,desc)
+  -- show the search and what found in the form with scrolling bars if needed
+  local formspec = vikidia.composeFormspec %
+    {
+      position = position,
+      search = search,
+      desc = minetest.formspec_escape(desc)
+    }
+  minetest.show_formspec(player_name, vikidia.formname, formspec)
+end
 
 minetest.register_node("vikidia:sign", {
   description = "cartello vikidia",
@@ -131,23 +78,34 @@ minetest.register_node("vikidia:sign", {
   groups = {oddly_breakable_by_hand = 1},
   on_rightclick = function(pos, node, player, itemstack, pointed_thing)
     local player_name = player:get_player_name()
-    local key = minetest.pos_to_string(pos)
-    local env = vikidia.nodeEnvs[key];
-    if not env then
-      env={}
-      env.search = ""
-      env.desc = ""
-      env.position = key
-      vikidia.nodeEnvs[key]=env
+    local position = minetest.pos_to_string(pos)
+    local key = getInfoText(position)
+    local desc = ""
+    if not(key == "") then
+        desc = callWikipedia(key)
     end
-
-    local formspec = vikidia.composeFormspec % {
-      position = key,
-      search = env.search,
-      desc = minetest.formspec_escape(env.desc)
-    }
-    minetest.log(formspec)
-    minetest.show_formspec(player_name, vikidia.formname, formspec)
+    showForm(player_name,position,key,desc)
   end
-
 })
+
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+  -- be sure to check this is really our formname to not pollute and receive others
+  if formname ~= vikidia.formname then
+    return false
+  end
+  -- if exit button just exits
+  if fields.exit then
+    minetest.log("form quit")
+    return false
+  end
+  local position = fields.position
+  local search = fields.search
+  local desc = callWikipedia(search)
+  if(not desc) then
+    desc = "Questa voce su Vikidia non esiste, prova a scriverla!"
+  else
+    setInfoText(position,search)
+  end
+  local player_name=player:get_player_name()
+  showForm(player_name,position,search,desc)
+end)
